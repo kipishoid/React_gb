43ac5ce91f6b62fc7a969edf399fbd87ab48734a
[sivolapov_max] <[kipish.max@mail.ru]>
2022-02-15 21:28:31 +0300

    lesson2

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,13 +1,39 @@
+import { useState, useEffect } from "react";
 import "./App.sass";
-import { Message } from "./components/Message";
+import { Message } from "./components/Message/index.js";
+import { Form } from "./components/Form";
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = (txt) => {
+    const newMessage = {
+      txt,
+      author: "user",
+    };
+    setMessageList((prevMessageList) => [...prevMessageList, newMessage]);
+  };
+
+  useEffect(() => {
+    if (messageList[messageList.length - 1]?.author === "user") {
+      const newMessage = {
+        txt: "Hello, i'm bot",
+        author: "bot",
+      };
+      setTimeout(() => {
+        setMessageList((prevMessageList) => [...prevMessageList, newMessage]);
+      }, 1500);
+    }
+  }, [messageList]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Hello</h3>
-        <Message text="My First React App" />
-      </header>
+      <div className="App__content">
+        {messageList.map((message) => (
+          <Message txt={message.txt} author={message.author} />
+        ))}
+      </div>
+      <Form onSubmit={sendMessage} name="Send"></Form>
     </div>
   );
 }
